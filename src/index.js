9e3430dad70bce32e5a3e8a4616abf1b8c950c95
Ricardo Mendes <ricardolsmendes@gmail.com>
2019-12-30 22:42:03 -0300

    Add initial version (#1)

---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { RawToNDJsonGCSFileConverter } = require('./raw-to-ndjson-gcs-file-converter');
+
+module.exports = { RawToNDJsonGCSFileConverter: RawToNDJsonGCSFileConverter };
